0393d7194a68292f11ba064bbcf22c91bd282818
maitreebain <maitreebain@gmail.com>
2019-10-29 17:02:18 -0400

    Initial Commit

---
+++ Color-Guessing-Game/Colors.swift
@@ -0,0 +1,11 @@
+//
+//  Colors.swift
+//  Color-Guessing-Game
+//
+//  Created by Maitree Bain on 10/29/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+
---
+++ Color-Guessing-Game/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Color-Guessing-Game
+//
+//  Created by Maitree Bain on 10/29/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+//    let myColor = UIColor(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)
+//
+    @IBOutlet weak var imagePrompt: UIImageView!
+    
+    
+//update at index
+    
+    @IBAction func optionButtons(_ colorButton: UIButton) {
+        
+    }
+    
+    
+    
+    
+}
+
